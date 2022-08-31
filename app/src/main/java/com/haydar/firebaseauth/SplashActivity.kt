600a29c1d85755e691dbf7eade2983e11d14d9a5
haydarahmad <fattanhay0@gmail.com>
2022-08-31 13:42:55 +0700

    initial commit

---
+++ app/src/main/java/com/haydar/firebaseauth/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.haydar.firebaseauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
